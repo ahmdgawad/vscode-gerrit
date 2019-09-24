cfffcfeac048a1bfd56b6ac4c0ec9f49d9fd21f8
刘培 <liupei01@fenbi.com>
2019-09-24 15:00:57 +0800

    DEL:删除 test 文件:

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: liupei 
+ * @Date: 2019-09-24 15:00:07 
+ * @Last Modified by: liupei
+ * @Last Modified time: 2019-09-24 15:00:30
+ */
+
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
